d29a74c994eb8afcb66f4a806f3b7a845cb7367e
Rohit Koli <85403005+Rohit-Koli@users.noreply.github.com>
2023-12-11 21:16:20 +0530

    11-12-2023:21:16

--- script.js
+++ script.js
@@ -29,7 +29,20 @@ function MovieSearch(mvname){
 		<p class="release">Release Year :${ry}</p>
 		</li></ul>
 		</div>`
-		document.querySelector('.mv').innerHTML+=movie;
+		const mo=`<div class="card text-center">
+		<div class="card-header">
+		${name}
+		</div>
+		<div class="card-body">
+		  <h5 class="card-title"><img src="${poster}height="250px" width="250px"></h5>
+		  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+		  <a href="#" class="btn btn-primary">Go somewhere</a>
+		</div>
+		<div class="card-footer text-muted">
+		  2 days ago
+		</div>
+	  </div>`
+		document.querySelector('.mv').innerHTML+=mo;
 	})
 })
 }
